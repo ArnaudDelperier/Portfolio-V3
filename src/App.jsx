e7953082187438942398307a7784ac927201cb7e
ArnaudDelperier <arnaud.delperier@gmail.com>
2022-01-14 13:09:19 +0100

    homepage structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import './app.scss';
+import Home from './components/home/Home'
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,32 @@
 import "./home.scss"
+import "animate.css"
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
         <div className="home">
-            
+            <div className="main__title">
+                <div className="animate__animated animate__lightSpeedInRight" >Welcome</div>
+                <div className="animate__animated animate__bounceIn" style={{animationDelay: "0.3s"}}>to</div>
+                <div className="wrapper">
+                    <div className="my__word animate__animated animate__lightSpeedInLeft" style={{animationDelay: "0.5s"}}>my</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "0.6s"}}>P</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "0.7s"}}>o</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "0.8s"}}>r</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "0.9s"}}>t</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "1s"}}>f</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "1.1s"}}>o</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "1.2s"}}>l</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "1.3s"}}>i</div>
+                    <div className="animate__animated animate__bounceIn" style={{animationDelay: "1.4s"}}>o</div>
+                </div>
+            </div>
+            <div className="link__wrapper">
+                <Link className="link animate__animated animate__fadeIn" style={{animationDelay: "1.5s"}} to="about">A propos de moi</Link>
+                <Link className="link animate__animated animate__fadeIn" style={{animationDelay: "1.7s"}} to="projects">Mes projects</Link>
+                <Link className="link animate__animated animate__fadeIn" style={{animationDelay: "1.8s"}} to="contact">Contactez moi</Link>
+            </div>
+
         </div>
     )
 }
