2f4d826f16187ff6d66c019eccc1b3cdfe69c870
ArnaudDelperier <arnaud.delperier@gmail.com>
2022-01-20 18:42:57 +0100

    projects page done

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const projects = [
+    {
+        name: "Horror Footage Finder",
+        description: "Laissez vous embarquer dans le cinéma d'horreur avec notre outil de recommandation de films du genre du found footage.",
+        image: require('./assets/horrorFootageFinder.png'),
+        link: "https://horror-footage-finder.netlify.app/",
+        tags: ["Website", "React"]
+    },
+    {
+        name: "Bubbles Blaster",
+        description: 'Jeu réalisé dans le cadre d\'un challenge " 1 hour 1 game ". Détruisez les bulles avant qu\'elles ne vous écrasent dans ce mini jeu d\'arcade hardcore.',
+        image: require('./assets/bubblesblaster.png'),
+        link: "https://bubblesblaster.netlify.app/",
+        tags: ["Game", "Pico-8"]
+    },
+    {
+        name: "Pocket converter",
+        description: "Application de conversion de devise.",
+        image: require('./assets/pocket-converter.png'),
+        link: "https://pocket-converter.netlify.app/",
+        tags: ["App"]
+    },
+    {
+        name: "Shifumi Lizard Spock",
+        description: 'Jeu du pierre-papier-ciseaux revisité version "The Big Bang Theory". ',
+        image: require('./assets/shifumi-lizard-spock.png'),
+        link: "https://shifumi-lizard-spock.netlify.app/",
+        tags: ["App"]
+    },
+    {
+        name: "Work in progress",
+        description: 'De nouvelles applications arrivent très bientôt !". ',
+        image: require('./assets/work-in-progress.png'),
+        link: "none",
+        tags: []
+    },
+]
+
+export default projects;
